80db787dea359afa14d756a1cfa6dfb51f9b659c
Valeriy Nehovora <nehovora.val@gnail.com>
2022-03-19 22:03:57 -0400

    create EventList component

--- my-next-react-app/src/App.js
+++ my-next-react-app/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import React, { useState } from "react";
 import Title from "./components/Title";
 import Modal from "./components/Modal";
+import EventList from "./components/EventList";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -50,15 +51,7 @@ function App() {
         </div>
       )}
 
-      {showEvents &&
-        events.map((event, index) => (
-          <React.Fragment key={event.id}>
-            <h2>
-              {index + 1} - {event.title}
-            </h2>
-            <button onClick={() => handleClick(event.id)}>delete event</button>
-          </React.Fragment>
-        ))}
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
 
       {/* <Modal>
         <h2>10% Off Cupone Code!</h2>
