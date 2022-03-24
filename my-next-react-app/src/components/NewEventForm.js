83d9f7e36781c2a59689c83abc5fb4dbdd3856aa
Valeriy Nehovora <nehovora.val@gnail.com>
2022-03-23 22:45:59 -0400

    select boxes

--- my-next-react-app/src/components/NewEventForm.js
+++ my-next-react-app/src/components/NewEventForm.js
@@ -4,10 +4,12 @@ import React, { useState, useRef } from "react";
 export default function NewEventForm({ addEvent }) {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
+  const [location, setLocation] = useState("manchester");
 
   const resetForm = () => {
     setTitle("");
     setDate("");
+    setLocation("manchester");
   };
 
   const handleSubmit = e => {
@@ -16,8 +18,11 @@ export default function NewEventForm({ addEvent }) {
     const event = {
       title,
       date,
+      location,
       id: Math.floor(Math.random() * 10000),
     };
+    console.log(event);
+
     addEvent(event);
     resetForm();
   };
@@ -39,6 +44,14 @@ export default function NewEventForm({ addEvent }) {
           onChange={e => setDate(e.target.value)}
           value={date}
         ></input>
+      </label>
+      <label>
+        <span>Event location: </span>
+        <select onChange={e => setLocation(e.target.value)}>
+          <option value="manchester">Manchecter</option>
+          <option value="iondon">London</option>
+          <option value="cardiff">Cardiff</option>
+        </select>
       </label>
       <button>Submite</button>
     </form>
