eb727c298846179f93312fd3ab100eb751f91a35
Valeriy Nehovora <nehovora.val@gnail.com>
2022-03-23 20:44:26 -0400

    controlled input

--- my-next-react-app/src/components/NewEventForm.js
+++ my-next-react-app/src/components/NewEventForm.js
@@ -5,23 +5,33 @@ export default function NewEventForm() {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
 
-  // const handleChange = e => {
-  //   setTitle(e.target.value);
-  // };
+  const resetForm = () => {
+    setTitle("");
+    setDate("");
+  };
   return (
     <form className="new-event-form">
       <lable>
         <span>Event Title:</span>
-        <input type="text" onChange={e => setTitle(e.target.value)}></input>
+        <input
+          type="text"
+          onChange={e => setTitle(e.target.value)}
+          value={title}
+        ></input>
       </lable>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={e => setDate(e.target.value)}></input>
+        <input
+          type="date"
+          onChange={e => setDate(e.target.value)}
+          value={date}
+        ></input>
       </label>
       <button>Submite</button>
       <p>
         title - {title}, date - {date}
       </p>
+      <p onClick={resetForm}>Reset The Form</p>
     </form>
   );
 }
