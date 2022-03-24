0ba47949c7eec881569a3decaec51edacaf542a0
Valeriy Nehovora <nehovora.val@gnail.com>
2022-03-23 22:29:40 -0400

    back to previous congition

--- my-next-react-app/src/components/NewEventForm.js
+++ my-next-react-app/src/components/NewEventForm.js
@@ -2,23 +2,20 @@ import "./NewEventForm.css";
 import React, { useState, useRef } from "react";
 
 export default function NewEventForm({ addEvent }) {
-  // const [title, setTitle] = useState("");
-  // const [date, setDate] = useState("");
-  const title = useRef();
-  const date = useRef();
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
 
   const resetForm = () => {
-    title.current.value = "";
-    date.current.value = "";
+    setTitle("");
+    setDate("");
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(title, date);
 
     const event = {
-      title: title.current.value,
-      date: date.current.value,
+      title,
+      date,
       id: Math.floor(Math.random() * 10000),
     };
     addEvent(event);
@@ -31,18 +28,16 @@ export default function NewEventForm({ addEvent }) {
         <span>Event Title:</span>
         <input
           type="text"
-          ref={title}
-          // onChange={e => setTitle(e.target.value)}
-          // value={title}
+          onChange={e => setTitle(e.target.value)}
+          value={title}
         ></input>
       </label>
       <label>
         <span>Event Date:</span>
         <input
           type="date"
-          ref={date}
-          // onChange={e => setDate(e.target.value)}
-          // value={date}
+          onChange={e => setDate(e.target.value)}
+          value={date}
         ></input>
       </label>
       <button>Submite</button>
